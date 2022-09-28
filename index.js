8e4bd76f5182a3f5bf39d7b9988c3183454f7d21
maurice diong <mdiong00@gmail.com>
2022-09-27 21:39:56 -0400

    commit tree 4f7d2efc59228d897ae622967ea4bbd205bb7a17

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+const distanceFromHqInBlocks = function(pickup) {
+    return Math.abs(pickup - 42);
+}
+
+const distanceFromHqInFeet = function(blocks) {
+    return distanceFromHqInBlocks (blocks) * 264;
+}
+
+const distanceTravelledInFeet = function(startBlock,destinationBlock) {
+    return Math.abs(destinationBlock-startBlock)*264;
+}
+
+const calculatesFarePrice = function (start,destination) {
+    const distance = distanceTravelledInFeet (start,destination);
+    if (distance >= 2500) {
+        return `cannot travel that far`;
+    }
+    else if (distance <400) {
+        return 0;
+    }
+    else if (distance <= 2000) {
+        return (distance-400)*0.02;
+    }
+    
+    return  25;
+}
